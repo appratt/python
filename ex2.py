a37f7c644a1eb532a3d8fa1320547c78168749cb
Andrew Pratt <appratt@gmail.com>
2012-06-11 23:54:16 -0400

    ex3 - math practice

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later...
+# Anything after the # is ignored by Python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comma to "disable" or comment out a piece of code.
+# print "This won't run."
+
+print "This will run."
